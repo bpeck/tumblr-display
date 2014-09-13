cc8d756a938a1bc88d386ad7b8d9ffd5807bad20
Ben Peck <bvpeck@gmail.com>
2014-09-13 12:09:35 -0700

    remove old files from flask backend

--- src/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-uwsgi --http :8000 --wsgi-file webMain.py --callable flask_app --catch-exceptions & python main.py
--- src/webMain.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import multiprocessing
-from web.Backend import Backend
-
-info_queue = multiprocessing.queues.Queue()
-command_queue = multiprocessing.queues.SimpleQueue()
-flask_app = Flask('web')
-
-Backend(command_queue, info_queue, flask_app, host='localhost', port=8000, debug=True)
